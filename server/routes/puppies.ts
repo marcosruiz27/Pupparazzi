1fc40daa67d6e5a8382563594266dcfb984d35f0
Marcos Ruiz <marcosruiz27@gmail.com>
2024-11-05 17:55:25 +1300

    Add Display for single puppy page

--- server/routes/puppies.ts
+++ server/routes/puppies.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { getPuppyById } from '../../store.ts'
 import * as fs from 'node:fs/promises'
 
 const router = express.Router()
@@ -7,6 +8,16 @@ router.get('/', async (req, res) => {
   const json = await fs.readFile('./storage/data.json', 'utf8')
   const data = JSON.parse(json)
   res.json(data)
+})
+
+router.get('/:id', async (req, res) => {
+  console.log('Route: get /:id')
+  const id = Number(req.params.id)
+
+  const puppy = await getPuppyById(id)
+  if (!puppy) return res.sendStatus(404)
+
+  res.json(puppy)
 })
 
 export default router
--- store.ts
+++ store.ts
@@ -1,1 +1,15 @@
-// TODO: Write your fs functions that affect the puppy data in this file and export them. 
+// TODO: Write your fs functions that affect the puppy data in this file and export them.
+
+import type { Puppy } from './models/Puppy.ts'
+import * as fs from 'node:fs/promises'
+
+export async function getPuppyById(id: number): Promise<Puppy | undefined> {
+  const puppies = await getAllPuppies()
+  return puppies.find((puppy) => puppy.id === id)
+}
+
+export async function getAllPuppies(): Promise<Puppy[]> {
+  const json = await fs.readFile('./storage/data.json', 'utf8')
+  const data = JSON.parse(json)
+  return data.puppies
+}
